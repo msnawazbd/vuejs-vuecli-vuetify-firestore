41f9ac5a77d5f37ef3665172206b6e12cb491741
MS Nawaz <msnawazbd@gmail.com>
2019-09-01 18:31:04 +0600

    Initial Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+<nav>
+    <v-toolbar>
+      <v-icon @click="drawer = !drawer">mdi-apps</v-icon>
+      <v-toolbar-title class="px-2">Title</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>
+
+    <v-navigation-drawer absolute temporary :mini-variant.sync="mini" v-model="drawer">
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+                router :to="item.route"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+        return {
+          drawer: false,
+          mini: false,
+          items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard', route: '/' },
+          { title: 'Photos', icon: 'mdi-image', route: '/phpto' },
+          { title: 'About', icon: 'mdi-help-box', route: '/about' },
+        ],
+        right: null,
+        }
+  }
+};
+</script>
